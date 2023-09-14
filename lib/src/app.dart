5a7adea0854e02abdeca015994ded7f7888ae17e
raccoon91 <qsx314@gmail.com>
2023-09-15 01:49:35 +0900

    wip: supbase setup

---
+++ lib/src/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:raccoon_investment/src/pages/account_page.dart';
+import 'package:raccoon_investment/src/pages/login_page.dart';
+import 'package:raccoon_investment/src/pages/splash_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Raccoon Investment',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.green,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.green,
+          ),
+        ),
+      ),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/account': (_) => const AccountPage(),
+      },
+    );
+  }
+}
