f72cb920a669516b3664407d3a0543fe82324d78
raccoon91 <qsx314@gmail.com>
2023-10-02 00:55:32 +0900

    feat: modify list style

--- lib/widget/home/group_list.dart
+++ lib/widget/home/group_list.dart
@@ -11,17 +11,20 @@ class GroupList extends StatelessWidget {
     return BlocBuilder<GroupBloc, GroupState>(
       buildWhen: (prev, current) => current.status.isSuccess,
       builder: (context, state) {
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: state.groups.map((group) {
-              return GroupCard(group: group);
-            }).toList(),
+        return ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 160),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: state.groups.map((group) {
+                return GroupCard(group: group);
+              }).toList(),
+            ),
           ),
         );
       },
--- lib/widget/home/stock_list.dart
+++ lib/widget/home/stock_list.dart
@@ -10,42 +10,45 @@ class StockList extends StatelessWidget {
     return BlocBuilder<TradeBloc, TradeState>(
       buildWhen: (prev, current) => current.status.isSuccess,
       builder: (context, state) {
-        return Container(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-          ),
-          child: Column(
-            children: state.stockList.map(
-              (stock) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(stock.ticker),
-                          Text('${stock.count}'),
-                        ],
-                      ),
-                      const SizedBox(height: 4),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            stock.name,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          Text('\$ ${stock.price.toStringAsFixed(2)}'),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ).toList(),
+        return ConstrainedBox(
+          constraints: const BoxConstraints(minHeight: 160),
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+            ),
+            child: Column(
+              children: state.stockList.map(
+                (stock) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(stock.ticker),
+                            Text('${stock.count}'),
+                          ],
+                        ),
+                        const SizedBox(height: 4),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              stock.name,
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                            Text('\$ ${stock.price.toStringAsFixed(2)}'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
           ),
         );
       },
