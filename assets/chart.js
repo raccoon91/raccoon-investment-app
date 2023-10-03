a026d97916f76d3b786cec886efe8b3e3e1c6322
raccoon91 <qsx314@gmail.com>
2023-10-03 23:10:04 +0900

    feat: add webview communication

--- assets/chart.js
+++ assets/chart.js
@@ -1,9 +1,8 @@
-const white = "#e0e3e1";
-const black = "#181c1c";
-const green = "#81e6d9";
-const red = "#ffb4ab";
-const border = "#b4c4c4";
-
+const white = '#e0e3e1';
+const black = '#181c1c';
+const green = '#81e6d9';
+const red = '#ffb4ab';
+const border = '#374040';
 
 const chartOptions = {
   layout: {
@@ -16,26 +15,24 @@ const chartOptions = {
   },
 };
 
-const chart = LightweightCharts.createChart(document.getElementById('chart'), chartOptions);
+const candleChartOptions = {
+  upColor: red,
+  downColor: green,
+  borderVisible: false,
+  wickUpColor: red,
+  wickDownColor: green,
+};
 
-const candlestickSeries = chart.addCandlestickSeries({
-    upColor: red,
-    downColor: green,
-    borderVisible: false,
-    wickUpColor: red,
-    wickDownColor: green,
+window.addEventListener('flutterInAppWebViewPlatformReady', () => {
+  const chart = LightweightCharts.createChart(document.getElementById('chart'), chartOptions);
+
+  const candlestickSeries = chart.addCandlestickSeries(candleChartOptions);
+
+  window.flutter_inappwebview.callHandler('chartData').then((result) => {
+    console.log(JSON.stringify(result));
+
+    candlestickSeries.setData(result);
+    
+    chart.timeScale().fitContent();
+  });
 });
-candlestickSeries.setData([
-    { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-    { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-    { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-    { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-    { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-    { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-    { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-]);
-
-chart.timeScale().fitContent();
--- lib/screen/chart_view_screen.dart
+++ lib/screen/chart_view_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:raccoon_investment/bloc/favorite/favorite_bloc.dart';
+import 'package:raccoon_investment/repository/favorite_repository.dart';
 
 class ChartViewScreen extends StatelessWidget {
   const ChartViewScreen({super.key});
 
   static Route<void> route() {
-    return MaterialPageRoute(builder: (context) => const ChartViewScreen());
+    return MaterialPageRoute(builder: (context) {
+      return BlocProvider(
+        create: (context) => FavoriteBloc(
+          favoriteRepository: FavoriteRepository(),
+        ),
+        child: const ChartViewScreen(),
+      );
+    });
   }
 
   @override
@@ -15,12 +25,54 @@ class ChartViewScreen extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(top: 24),
-          child: InAppWebView(
-            initialFile: 'assets/chart.html',
-            initialOptions: InAppWebViewGroupOptions(
-              crossPlatform: InAppWebViewOptions(transparentBackground: true),
-            ),
+          padding: const EdgeInsets.only(top: 12),
+          child: BlocBuilder<FavoriteBloc, FavoriteState>(
+            buildWhen: (previous, current) {
+              return current.status.isSuccess;
+            },
+            builder: (context, state) {
+              return InAppWebView(
+                initialFile: 'assets/chart.html',
+                initialOptions: InAppWebViewGroupOptions(
+                  crossPlatform: InAppWebViewOptions(
+                    transparentBackground: true,
+                  ),
+                ),
+                onWebViewCreated: (controller) {
+                  controller.addJavaScriptHandler(
+                    handlerName: 'chartData',
+                    callback: (args) {
+                      return [
+                        {
+                          'time': '2018-12-22',
+                          'open': 75.16,
+                          'high': 82.84,
+                          'low': 36.16,
+                          'close': 45.72
+                        },
+                        {
+                          'time': '2018-12-23',
+                          'open': 45.12,
+                          'high': 53.90,
+                          'low': 45.12,
+                          'close': 48.09
+                        },
+                        {
+                          'time': '2018-12-24',
+                          'open': 60.71,
+                          'high': 60.71,
+                          'low': 53.39,
+                          'close': 59.29
+                        },
+                      ];
+                    },
+                  );
+                },
+                onConsoleMessage: (controller, consoleMessage) {
+                  print(consoleMessage.message);
+                },
+              );
+            },
           ),
         ),
       ),
